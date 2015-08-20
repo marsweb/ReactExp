0f03b1e312b828ccac911e8f4df8b5912f6c0dad
marsweb <marswebb@qq.com>
2015-08-20 11:50:16 +0800

    new react example

---
+++ js/ReactTodoList.js
@@ -0,0 +1,29 @@
+var MyUl=React.createClass({
+	render:function(){
+		var createUl=function(item){
+			return <li>{item}</li>;
+		};
+		return <ul>{this.props.items.map(createUl)}</ul>
+	}
+});
+var MyComp=React.createClass({
+	getInitialState:function(){
+		return {items:["on1","on2"],inputText:""};
+	},
+	onChange:function(e){
+		this.setState({inputText:e.target.value});
+	},
+	onClick:function(){
+		this.setState({items:this.state.items.concat(this.state.inputText),inputText:""});
+	},
+	render:function(){
+		return (
+			<div>
+				<MyUl items={this.state.items}></MyUl>
+				<input onChange={this.onChange} value={this.state.inputText}/>
+				<button onClick={this.onClick}>添加</button>
+			</div>
+		);
+	}
+});
+React.render(<MyComp />, document.querySelector(".content"));
